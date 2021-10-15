96d00b0b4f3084855bb086a32759a7b0e5d72e86
Aditya bohra <adityabohra619@gmail.com>
2021-10-16 02:28:08 +0530

    Done

--- django_chat/main/react/minimized-chat/src/components/App/ChatList/Component.js
+++ django_chat/main/react/minimized-chat/src/components/App/ChatList/Component.js
@@ -1,16 +1,13 @@
-import React from 'react';
+import React from "react";
 //import {} from './styles.js'
-import {StyleWebsocketStatus, StyledDataList} from './styles';
+import { StyleWebsocketStatus, StyledDataList } from "./styles";
 
-
-const Component=(props)=>{
-    return(
+const Component = (props) => {
+  return (
     <StyledDataList>
       {props.status}
       {props.dataList}
     </StyledDataList>
-
-    )
-
-}
+  );
+};
 export default Component;
--- django_chat/main/react/minimized-chat/src/components/App/ChatList/Wrapper.js
+++ django_chat/main/react/minimized-chat/src/components/App/ChatList/Wrapper.js
@@ -1,53 +1,55 @@
-import React,{useEffect,useState} from 'react';
-import Component from './Component';
-import Message from './../Message';
-import Room from './../Room';
+import React, { useEffect, useState } from "react";
+import Component from "./Component";
+import Message from "./../Message";
+import Room from "./../Room";
 
-
-const Wrapper=(props)=>{
+const Wrapper = (props) => {
   var dataList = [];
   const [isLoading, setIsLoading] = useState(true);
   const [chatStatus, setChatStatus] = useState(props.status);
   console.log(props.data);
   console.log(props);
   if (props.type) {
-    dataList = props.data.map(d => (
+    dataList = props.data.map((d) => (
       <Room onClick={props.onClick} name={d.name} id={d.id} />
     ));
   } else {
-    if(props.data[0].text!=undefined){
-    dataList = props.data.map(data => (
-      <Message
-        onClick={props.onClick}
-        id={data.id}
-        text={data.text}
-        file={data.file}
-        time={data.date_created}
-        by={data.sender__username}
-        user={props.user}
-      />
-    ));
+    if (props.data[0].text != undefined) {
+      dataList = props.data.map((data) => (
+        <Message
+          onClick={props.onClick}
+          id={data.id}
+          text={data.text}
+          file={data.file}
+          time={data.date_created}
+          by={data.sender__username}
+          user={props.user}
+        />
+      ));
+    } else {
+      dataList = (
+        <p style={{ padding: "10px", textAlign: "center" }}>
+          No Messages to Show
+        </p>
+      );
+    }
   }
-      else{
-         dataList= <p style={{padding:'10px',textAlign:'center'}}>No Messages to Show</p>
-      }
-
-  }
-  const scrollToBottom = event => {
-    var list = document.getElementById('chat').children[0];
+  const scrollToBottom = (event) => {
+    var list = document.getElementById("chat").children[0];
     list.scrollTop = list.scrollTopMax;
   };
-  const scrollToTop = event => {
-    var list = document.getElementById('chat').children[0];
+  const scrollToTop = (event) => {
+    var list = document.getElementById("chat").children[0];
     list.scrollTop = 0;
   };
   const status = () => {
-    console.log('chatStatus' + chatStatus);
+    console.log("chatStatus" + chatStatus);
     if (chatStatus == 0) {
       return (
         <StyleWebsocketStatus
           status={chatStatus}
-          style={{background: '#c9ec96'}}>
+          style={{ background: "#c9ec96" }}
+        >
           Connecting
         </StyleWebsocketStatus>
       );
@@ -73,16 +75,15 @@ const Wrapper=(props)=>{
     setChatStatus(props.status);
   }, [props.status]);
 
-
   useEffect(() => {
     //setIsLoading(false);
     if (props.user && !isLoading) {
-      console.log('Data is here $$');
+      console.log("Data is here $$");
       scrollToBottom();
     } else {
       scrollToTop();
     }
-    console.log('-------------------------------------------------');
+    console.log("-------------------------------------------------");
   }, [props.data, isLoading]);
   useEffect(() => {
     if (isLoading) {
@@ -92,15 +93,10 @@ const Wrapper=(props)=>{
 
   useEffect(() => {
     setChatStatus(props.status);
-    console.log('setting chatStatus');
+    console.log("setting chatStatus");
   }, [props.status]);
 
-
-return(
-
-<Component {...props} dataList={dataList} status={status}/>
-)
-}
-
+  return <Component {...props} dataList={dataList} status={status} />;
+};
 
 export default Wrapper;
