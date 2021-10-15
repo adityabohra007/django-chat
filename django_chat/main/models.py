3472c614f53e7e7680f866292763524f5c35ddb5
Aditya bohra <adityabohra619@gmail.com>
2021-10-15 23:24:05 +0530

    DOne

--- django_chat/main/models.py
+++ django_chat/main/models.py
@@ -1,90 +1,87 @@
-import uuid
-import time
-import os
 import asyncio
-from django.contrib.auth.models import User
-from django.db.models.signals import m2m_changed,post_save,pre_delete,post_delete
-from django.conf import settings
-from django.db import models
-from channels.layers import get_channel_layer
-from asgiref.sync import async_to_sync
-from  django.contrib.auth.models import User
-
-
-
-from django.contrib.auth.models import User
-from channels.layers import get_channel_layer
-from .serializers_chat import ChatMemberSerializer
 import json
+import os
+import time
+import uuid
+
 from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import (m2m_changed, post_delete, post_save,
+                                      pre_delete)
+
+from .serializers_chat import ChatMemberSerializer
+
+
 def send_user_status(user):
     room_list = User.objects.filter(id=user.id).values("room")
-    channel_layer=get_channel_layer()
-    data={"type":"send.websocket",
-          "message":"update",
-          "update_user":json.loads(json.dumps(ChatMemberSerializer(User.objects.filter(id=user.id),many=True).data))
-          }
+    channel_layer = get_channel_layer()
+    data = {"type": "send.websocket",
+            "message": "update",
+            "update_user": json.loads(json.dumps(ChatMemberSerializer(User.objects.filter(id=user.id), many=True).data))
+            }
     print("send_user_utils")
     try:
         print(data)
         # time.sleep(5)
-        company=user.usercompany.company.id
+        company = user.usercompany.company.id
         print(company)
-        clients=None
+        clients = None
     except:
         print("yo found the prob")
 
     try:
-        clients=Client.objects.filter(user__usercompany__company=company)
+        clients = Client.objects.filter(user__usercompany__company=company)
         print("from custom_utils send update")
         print(clients)
     except:
-        clients=Client.objects.all()
+        clients = Client.objects.all()
         print(clients)
         print("client error")
 
     if clients.exists():
         for client in clients:
-            group_name="users_"+str(company)
+            group_name = "users_"+str(company)
             try:
                 async_to_sync(channel_layer.group_send)(group_name, data)
             except:
                 print("Error group_send")
     else:
         print("no one is there for you")
-
 
 
 def send_user_remove_status(user):
     ''' Delete to Channels: Removed User update to the chat room'''
-    print(user,"send_remove")
+    print(user, "send_remove")
     room_list = User.objects.filter(id=user.id).values("room")
-    channel_layer=get_channel_layer()
-    data={"type":"send.websocket",
-          "message":"remove",
-          "update_user":json.loads(json.dumps(ChatMemberSerializer(User.objects.filter(id=user.id),many=True).data))
-          }
+    channel_layer = get_channel_layer()
+    data = {"type": "send.websocket",
+            "message": "remove",
+            "update_user": json.loads(json.dumps(ChatMemberSerializer(User.objects.filter(id=user.id), many=True).data))
+            }
     try:
         print(data)
         # time.sleep(5)
-        company=user.usercompany.company.id
+        company = user.usercompany.company.id
         print(company)
-        clients=None
+        clients = None
     except:
         print("yo found the prob")
 
     try:
-        clients=Client.objects.filter(user__usercompany__company=company)
+        clients = Client.objects.filter(user__usercompany__company=company)
         print("from custom_utils send update")
         print(clients)
     except:
-        clients=Client.objects.all()
+        clients = Client.objects.all()
         print(clients)
         print("client error")
 
     if clients.exists():
         for client in clients:
-            group_name="users_"+str(company)
+            group_name = "users_"+str(company)
             try:
                 async_to_sync(channel_layer.group_send)(group_name, data)
             except:
@@ -93,30 +90,30 @@ def send_user_remove_status(user):
         print("no one is there for you")
 
 
-
-
-
 class Client(models.Model):
-    user=models.ForeignKey(User,default=1,on_delete=models.CASCADE)
-    last_seen=models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+    last_seen = models.DateTimeField(auto_now_add=True)
 
-def users_status(sender,instance,**kwargs):
-    channel_layer=get_channel_layer()
+
+def users_status(sender, instance, **kwargs):
+    channel_layer = get_channel_layer()
     send_user_status(instance.user)
 
-def update_remove(sender,instance,**kwargs):
+
+def update_remove(sender, instance, **kwargs):
     ''' Delete Signal: Removed User update to the chat room'''
-    channel_layer=get_channel_layer()
+    channel_layer = get_channel_layer()
     send_user_remove_status(instance.user)
 
 
-post_save.connect(users_status,sender=Client) #post_save user update add
-post_delete.connect(update_remove,sender=Client) # post_delete user update delete
+post_save.connect(users_status, sender=Client)  # post_save user update add
+# post_delete user update delete
+post_delete.connect(update_remove, sender=Client)
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
-            on_delete=models.CASCADE, related_name='profile')
+                                on_delete=models.CASCADE, related_name='profile')
     last_visit = models.DateTimeField()
 
 
@@ -131,10 +128,10 @@ class DateTimeModel(models.Model):
 
 class Room(DateTimeModel):
     id = models.UUIDField(primary_key=True,
-            default=uuid.uuid4,
-            editable=False)
+                          default=uuid.uuid4,
+                          editable=False)
     members = models.ManyToManyField(settings.AUTH_USER_MODEL)
-    name = models.CharField(max_length=100,default="Discussion Room")
+    name = models.CharField(max_length=100, default="Discussion Room")
     # def __str__(self):
     #     memberset = self.members.all()
     #     members_list = []
@@ -144,37 +141,43 @@ class Room(DateTimeModel):
     #     return "("+self.name+") "+", ".join(members_list)
 
     def group_name(self):
-        print("chat_",self.id)
-def room_member_status(sender,instance, **kwargs):
+        print("chat_", self.id)
+
+
+def room_member_status(sender, instance, **kwargs):
     print(instance.id)
-    room_id=instance.id
-    room_group_name='chat_%s' % room_id
-    members=instance.members.all()
-    print("--",members,room_group_name,"--")
-    #user_status(room_group_name)
-#m2m_changed.connect(room_member_status,sender=Room.members.through)
+    room_id = instance.id
+    room_group_name = 'chat_%s' % room_id
+    members = instance.members.all()
+    print("--", members, room_group_name, "--")
+    # user_status(room_group_name)
+# m2m_changed.connect(room_member_status,sender=Room.members.through)
+
 
 def user_status(room_group_name):
 
     channel_layer = get_channel_layer()
     print("--user_status--")
-    data={"type":"user_status","message":"members"}
+    data = {"type": "user_status", "message": "members"}
     #async_to_sync(channel_layer.group_send)(room_group_name, data)
+
 
 class Message(DateTimeModel):
     sender = models.ForeignKey(settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE, related_name='sender')
-    file = models.FileField(null=True,blank=True)
+                               on_delete=models.CASCADE, related_name='sender')
+    file = models.FileField(null=True, blank=True)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     text = models.TextField(null=True)
     recipients = models.ManyToManyField(settings.AUTH_USER_MODEL,
-        related_name='recipients')
+                                        related_name='recipients')
+
     class Meta:
         ordering = ['-id']
+
     def __str__(self):
         return str(self.room.id)+"sender"+self.sender.username
 
-#def new_message(sender,instance,**kwargs):
+# def new_message(sender,instance,**kwargs):
 #    if instance.file is not None:
 #        basename = os.path.basename( instance.file.path )
 #        channel=get_channel_layer()
@@ -185,9 +188,7 @@ class Message(DateTimeModel):
 #        async_to_sync(channel.group_send)(group_id,data)
 
 
-#post_save.connect(new_message,sender=Message)
+# post_save.connect(new_message,sender=Message)
 
-    #def __str__(self):
+    # def __str__(self):
     #    return f'{self.text} sent by "{self.sender}" in Room "{self.room}"'
-
-
